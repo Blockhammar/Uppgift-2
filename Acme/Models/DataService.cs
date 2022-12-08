3715ccd6126da67884dafe15f2aa548bb44b8d75
blockkhammar <blockhammar.programmering@gmail.com>
2022-12-08 10:04:31 +0100

    Add project files.

---
+++ Acme/Models/DataService.cs
@@ -0,0 +1,33 @@
+﻿namespace Uppgift2.Models
+{
+    public class DataService
+    {
+        // "Fake" DB
+        static List<Dog> dogs = new List<Dog>
+        {
+            new Dog { Id = 20, Name = "Taylor", Age = 5},
+            new Dog { Id = 10, Name = "Gibson", Age = 5},
+            new Dog { Id = 30, Name = "Roland", Age = 5},
+        };
+
+        public Dog[] GetAllDogs()
+        {
+            return dogs
+                .OrderBy(o => o.Name)
+                .ThenBy(o => o.Age)
+                .ToArray();
+        }
+
+        public void Add(Dog dog)
+        {
+            dog.Id = dogs.Max(o => o.Id) + 1;
+            dogs.Add(dog);
+        }
+
+        public Dog GetDogById(int id)
+        {
+            return dogs
+                 .FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
